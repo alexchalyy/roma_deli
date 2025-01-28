8f55d226d2cdbd1a26120a0f18264c85247b5582
alexchalyy <alexchalyy@gmail.com>
2025-01-28 15:07:52 -0500

    basic version

---
+++ Migrations/20250114162303_AddCityStateZipCode.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCityStateZipCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "State",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ZipCode",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ZipCode",
+                table: "AspNetUsers");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# roma_deli
+# Roma Deli
+
+# Description
+
+This is a deli website.
+
+![home](AUTHAPP/wwwroot/images/home.PNG)
+
+When the user opens the page, home page is opened for Roma Deli. It has links to itself, menu, register, and log in pages.
+
+![menu](AUTHAPP/wwwroot/images/menu.PNG)
+
+If the user clicks on menu page, menu page is opened with all menu items. It includes picture and description.
+
+![log_in](AUTHAPP/wwwroot/images/log_in.PNG)
+
+If user clicks on log in link, log in page is opened with username and password field where user can log in.
+
+![register](AUTHAPP/wwwroot/images/register.PNG)
+
+If the user clicks on register page, registration page is opened where user can register with first name, last name, phone number, e-mail, password, and address.
+
+# Creation Process
+
+The web site was written using .NET.
